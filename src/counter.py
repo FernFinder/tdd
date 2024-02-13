07d8f4e2f2cc2309137300ce3d70cc86e9c03c7b
FernFinder <fernandolrojas00@gmail.com>
2024-02-12 20:39:58 -0800

    Updated formating for linting with flake8

--- src/counter.py
+++ src/counter.py
@@ -14,26 +14,28 @@ def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
-    if name in COUNTERS:
-      return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    if name in COUNTERS: 
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """Update a Counter"""
     app.logger.info(f"Request to update a counter: {name}")
     global COUNTERS
-    if name in COUNTERS:
-      COUNTERS[name] = COUNTERS[name] + 1
-      return {name: COUNTERS[name]}, status.HTTP_200_OK
+    if name in COUNTERS: 
+        COUNTERS[name] = COUNTERS[name] + 1
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
     return {"Message":f"Counter {name} found"}, status.HTTP_404_NOT_FOUND
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
     """Get a Counter"""
     app.logger.info(f"Request to get a counter: {name}")
     global COUNTERS
-    if name in COUNTERS:
-      return {name: COUNTERS[name]}, status.HTTP_200_OK
+    if name in COUNTERS: 
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
     return {"Message":f"Counter {name} found"}, status.HTTP_404_NOT_FOUND
