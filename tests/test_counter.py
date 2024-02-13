0d59a5677dab454112ccf38715f98451725862e3
FernFinder <fernandolrojas00@gmail.com>
2024-02-12 20:17:36 -0800

    Added workflow

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,51 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+class CounterTest(TestCase):
+    def setUp(self):
+      self.client = app.test_client()
+
+    """Counter tests"""
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_updating_a_counter(self):
+        """It should update a counter"""
+        update = self.client.post('/counters/qux')
+        self.assertEqual(update.status_code, status.HTTP_201_CREATED)
+        baseline = update.json["qux"]
+        self.assertEqual(baseline, 0)
+        update = self.client.put('/counters/qux')
+        self.assertEqual(update.status_code, status.HTTP_200_OK)
+        self.assertEqual(update.json["qux"], baseline+1)
+
+    def test_updating_a_nonexistent_counter(self):
+        """It should return an error for updating nonexistent counter"""
+        update = self.client.put('/counters/qqq')
+        self.assertEqual(update.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_a_counter(self):
+        """It should get a counter"""
+        result = self.client.get('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_get_a_nonexistent_counter(self):
+        result = self.client.get('/counters/qqq')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
